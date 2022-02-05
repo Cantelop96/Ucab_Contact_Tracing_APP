cd0e6408887e8de326cbebd3157a2b9b571c6535
Cantelop96 <70656092+Cantelop96@users.noreply.github.com>
2022-02-05 17:44:43 -0400

    Se añadio el modulo bluetooth y se creo un metodo que no estoy seguro si funciona de scan con BL LE

--- app/src/main/java/com/christianantelo/ucabcovid_19contacttracing/Fragments/Main_View.kt
+++ app/src/main/java/com/christianantelo/ucabcovid_19contacttracing/Fragments/Main_View.kt
@@ -1,9 +1,13 @@
 package com.christianantelo.ucabcovid_19contacttracing.Fragments
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.christianantelo.ucabcovid_19contacttracing.R
@@ -11,6 +15,7 @@ import kotlinx.android.synthetic.main.fragment_main__view.*
 
 
 class Main_View : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +25,10 @@ class Main_View : Fragment() {
         return inflater.inflate(R.layout.fragment_main__view, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         main_boton_sintomas.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_main_View_to_formularioFragment)
         }
--- app/src/main/java/com/christianantelo/ucabcovid_19contacttracing/MainActivity.kt
+++ app/src/main/java/com/christianantelo/ucabcovid_19contacttracing/MainActivity.kt
@@ -1,11 +1,79 @@
 package com.christianantelo.ucabcovid_19contacttracing
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private val REQUEST_ENABLE_BT = 1
+    lateinit var m_pairedDevices:Set<BluetoothDevice>
+    private val bluetoothLeScanner = bluetoothAdapter!!.bluetoothLeScanner
+    private var scanning = false
+    private val handler = Handler(Looper.getMainLooper())
+    private val SCAN_PERIOD: Long = 10000 //define tiempo de scan 10 seg
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(bluetoothAdapter == null){
+            Toast.makeText(this, "Este equipo no soporta el uso de Bluetooth, lo cual es indispensable para el funcionamiento de la aplicacion.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(resultCode == Activity.RESULT_OK){
+                if(bluetoothAdapter!!.isEnabled){
+                    Toast.makeText(this, "Se a activado el Bluetooth.", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "No se ha activado el Bluetooth", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else if (resultCode == Activity.RESULT_CANCELED){
+                Toast.makeText(this, "Se ha cancelado la activacion del Bluetooth", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun scanLeDevice() {
+        if (!scanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                scanning = false
+                bluetoothLeScanner.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            scanning = true
+            bluetoothLeScanner.startScan(leScanCallback)
+        } else {
+            scanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+        }
+    }
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            val list:ArrayList<BluetoothDevice> = ArrayList()
+            list.add(result.device)
+            Log.i("Device",""+result.device)
+        }
+    }
+
 }
+
